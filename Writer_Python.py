418278edbdda74f38d7bf0810795ec1694beae95
Jaidan R <123023648+Jaiwaire@users.noreply.github.com>
2023-01-23 14:24:43 -0500

    Update Writer_Python.py

--- Writer_Python.py
+++ Writer_Python.py
@@ -5,19 +5,19 @@ while True:
   if not Beginning:
     #Checks if there isn't a beginning
     Beginning = input()
-    if Beggining ~= False:
+    if Beggining:
       print(Beginning)
   if Beginning:
     #Checks if there is a beginning
     if not PreviousAddon:
       #Checks if there isn't an Addon
       Addon = input()
-      if Addon ~= False:
+      if Addon:
         print(Beginning + " " + Addon)
         PreviousAddon = Addon
     elif PreviousAddon:
       #Checks if there is an Addon
       Addon = input()
-      if Addon ~= False:
+      if Addon:
         print(Beginning + " " + PreviousAddon + " " + Addon)
         PreviousAddon = Addon
